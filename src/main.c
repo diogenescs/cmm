5beff28b50248011e1c62b236a167cebb96ae9a6
rick <ricks2600@gmail.com>
2013-12-01 22:39:54 -0200

    Init buffer for lexical analysis

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "main.h"
 
 
 int main(int argc, char **argv)
 {
 
+  char *buffer;
+  size_t buffer_size;
   
   if (argc != 2)
   {
@@ -12,5 +18,50 @@ int main(int argc, char **argv)
     exit(EXIT_FAILURE);
   }
   
+  buffer_size = init_buffer(argv[1], &buffer);
+
   exit(EXIT_SUCCESS);
+}
+
+
+
+size_t init_buffer(const char *filename, char **buffer)
+{
+ 
+  size_t buffer_size, temp;
+  FILE *source_fp;
+
+  struct stat fileinfo;
+
+  if (stat(filename, &fileinfo) == -1)
+  {
+     perror("stat");
+     exit(EXIT_FAILURE);
+  }
+
+  buffer_size = fileinfo.st_size;
+  *buffer = (char *)malloc(buffer_size * sizeof(char));
+  if (!*buffer)
+  {
+    perror("malloc");
+    exit(EXIT_FAILURE);
+  }
+ 
+  source_fp = fopen(filename, "rb");
+  if (!source_fp)
+  {
+    perror("fopen");
+    exit(EXIT_FAILURE);
+  }
+
+  temp = fread(*buffer, 1, buffer_size, source_fp);
+  fclose(source_fp);
+
+  if (temp < buffer_size)
+  {
+    fprintf(stderr, "%s\n", "Error reading file");
+    exit(EXIT_FAILURE);
+  }
+
+  return buffer_size;
 }
---
+++ src/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+
+size_t init_buffer(const char *filename, char **buffer);
+
+#endif
