5801b97c324a94448ef95c048b844db8e53e360e
rick <ricks2600@gmail.com>
2013-12-07 01:44:06 -0200

    token type defined

--- src/main.c
+++ src/main.c
@@ -9,18 +9,18 @@
 int main(int argc, char **argv)
 {
 
-  char *buffer;
-  size_t buffer_size;
+    char *buffer;
+    size_t buffer_size;
   
-  if (argc != 2)
-  {
-    fprintf(stderr, "Usage: %s <source.cmm>\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
-  
-  buffer_size = init_buffer(argv[1], &buffer);
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <source.cmm>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-  exit(EXIT_SUCCESS);
+    buffer_size = init_buffer(argv[1], &buffer);
+
+    exit(EXIT_SUCCESS);
 }
 
 
@@ -28,40 +28,40 @@ int main(int argc, char **argv)
 size_t init_buffer(const char *filename, char **buffer)
 {
  
-  size_t buffer_size, tmp;
-  FILE *source_fp;
+    size_t buffer_size, tmp;
+    FILE *source_fp;
 
-  struct stat fileinfo;
+    struct stat fileinfo;
 
-  if (stat(filename, &fileinfo) == -1)
-  {
-     perror("stat");
-     exit(EXIT_FAILURE);
-  }
+    if (stat(filename, &fileinfo) == -1)
+    {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
 
-  buffer_size = fileinfo.st_size;
-  *buffer = (char *)malloc(buffer_size * sizeof(char));
-  if (!*buffer)
-  {
-    perror("malloc");
-    exit(EXIT_FAILURE);
-  }
- 
-  source_fp = fopen(filename, "rb");
-  if (!source_fp)
-  {
-    perror("fopen");
-    exit(EXIT_FAILURE);
-  }
+    buffer_size = fileinfo.st_size;
+    *buffer = (char *)malloc(buffer_size * sizeof(char));
+    if (!*buffer)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
 
-  tmp = fread(*buffer, 1, buffer_size, source_fp);
-  fclose(source_fp);
+    source_fp = fopen(filename, "rb");
+    if (!source_fp)
+    {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
 
-  if (tmp < buffer_size)
-  {
-    fprintf(stderr, "%s\n", "Error reading file");
-    exit(EXIT_FAILURE);
-  }
+    tmp = fread(*buffer, 1, buffer_size, source_fp);
+    fclose(source_fp);
 
-  return buffer_size;
+    if (tmp < buffer_size)
+    {
+        fprintf(stderr, "%s\n", "Error reading file");
+        exit(EXIT_FAILURE);
+    }
+
+    return buffer_size;
 }
